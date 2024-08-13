616f827ae6de8a99fd740547f39b586ae421430d
anant-jaffa <anant@jaffa-ai.com>
2024-08-13 09:22:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -198,4 +198,6 @@ def summarize():
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
